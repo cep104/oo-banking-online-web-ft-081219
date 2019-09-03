1beca2ed776ca18d7a58e0334439199fb2179f0c
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-03 21:41:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,7 @@ class Transfer
  end
  
  def reverse_transfer 
-  if @status == "complete"
+  if self.status == "complete"
        self.receiver.balance -= @amount 
        self.sender.balance += @amount
        @status = "reversed"
