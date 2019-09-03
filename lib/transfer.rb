4e1da287a79b035a87e53ed2233b78bc1e685b42
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-03 20:05:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,29 +12,16 @@ class Transfer
   sender.valid? && receiver.valid?
  end
  
-# def execute_transaction
-#   @sender.balance -= @amount 
-#   @receiver.balance += @amount
-#     @status = "complete"
-#   if @status = "complete"
-#     @amount = 0
-#   end
-#   if @sender.balance < @amount 
-#     @status = "reject"
-#     "Transaction rejected. Please check your account balance."
-#   end
-# end
-# end
-def execute_transaction
-withdrawal = @sender.balance -= @amount 
-deposit = @receiver.balance += @amount
-if valid? && sender.balance > amount && self.status == "pending"
-      sender.withdrawal(self.amount)
-      receiver.deposit(self.amount)
-      self.status = "complete"
-    else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    end
+ def execute_transaction
+   @sender.balance -= @amount 
+   @receiver.balance += @amount
+    @status = "complete"
+   if @status = "complete"
+    @amount = 0
   end
+  if @sender.balance < @amount 
+    @status = "reject"
+     "Transaction rejected. Please check your account balance."
   end
+ end
+end
