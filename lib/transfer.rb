53bb7bf4081b747e9daab1103f48d7218e66f8bb
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-03 19:17:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 class Transfer
   attr_accessor :reciever, :amount, :name
   
- def initilize(name,reciever,amount)
+ def initilize(reciever,amount)
    @reciever = reciever 
    @amount = amount
    @name = name
