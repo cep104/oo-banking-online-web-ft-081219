d6c1ce6a7bdcbe0373d9bb010f67a5fa8d99cdb0
cep104 <candicelizabethpeters@gmail.com>
2021-02-28 22:00:12 -0600

    commit tree be173b6750afb370ff3c149bfc71505e4e467ac5

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,18 @@ class Transfer
  def valid?
   sender.valid? && receiver.valid?
  end
- 
+#  def execute_transaction
+#   if valid? && @sender.balance > @amount && @status = "pending"
+#     @sender.balance -= @amount
+#     @receiver.deposit(@amount)
+#     #binding.pry
+#     @status = "complete"
+#   else
+#      @status = "rejected"
+#     "Transaction rejected. Please check your account balance."
+    
+#   end
+# end
  def execute_transaction
   if @sender.balance > amount && valid? && @status == "pending"
   @sender.balance -= @amount 
